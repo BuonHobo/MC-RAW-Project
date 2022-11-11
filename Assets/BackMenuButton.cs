e464032a62d90a442ae2ec33bdd0ff5e2e6a0364
Rainer Cabral Ilao <ilaorainer@gmail.com>
2022-11-12 00:19:10 +0100

    Modified Menu, Added Settings Menu

--- Assets/BackMenuButton.cs
+++ Assets/BackMenuButton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayButton : MonoBehaviour
+public class BackMenuButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
