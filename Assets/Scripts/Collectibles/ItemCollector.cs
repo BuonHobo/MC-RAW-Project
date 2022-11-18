afb9244cd67fee939148808d8af7d70d92560939
buonhobo <naturytropik@gmail.com>
2022-11-18 02:28:29 +0100

    Added support for multiple fruits in a level

--- Assets/Scripts/Collectibles/ItemCollector.cs
+++ Assets/Scripts/Collectibles/ItemCollector.cs
@@ -10,20 +10,26 @@ public class ItemCollector : MonoBehaviour
     //[SerializeField] private AudioSource collectionSoundEffect;
 
     public bool collected { get; private set; } = false;
+    private bool canCollect=true;
 
-    [SerializeField] private Image indicator;
+    [SerializeField] private FruitIndicator indicator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Collectible") && !collected)
+        if (collision.gameObject.CompareTag("Collectible") && !collected && canCollect)
         {
             //collectionSoundEffect.Play();
             var anim = collision.gameObject.GetComponent<Animator>();
             anim.SetBool("Animate", false);
             anim.SetTrigger("Collect");
             Destroy(collision.gameObject, 0.5f);
-            indicator.gameObject.SetActive(true);
-            collected = true;
+            collected=indicator.addFrutto(collision.gameObject);
+
+            canCollect=false;
+            Invoke("restoreCollect",0.5f);
         }
+    }
+    private void restoreCollect(){
+        canCollect=true;
     }
 }
---
+++ Assets/Scripts/UI/FruitIndicator.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FruitIndicator : MonoBehaviour
+{
+    [SerializeField] GameObject[] fruitIcons;
+    [SerializeField] float distance;
+    [SerializeField] Color inactive_color;
+    private GameObject[] icons;
+    private bool[] fruttipresi;
+    private int frutti = 0;
+    private int ottenuti = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frutti=fruitIcons.Length;
+        fruttipresi=new bool[frutti];
+        icons= new GameObject[frutti];
+
+        for (int i=0; i<frutti;i++){
+            fruttipresi[i]=false;
+
+            icons[i]=Instantiate(fruitIcons[i],transform);
+            icons[i].name=icons[i].name.Split('(')[0];
+            icons[i].transform.position+= new Vector3(i*distance,0,0);
+            icons[i].GetComponent<Image>().color=inactive_color;
+        }
+    }
+
+    public bool addFrutto(GameObject frutto){
+
+        ottenuti++;
+
+        for (int i=0;i<frutti;i++){
+            if (fruttipresi[i]) continue;
+
+        Debug.Log(icons[i].name+frutto.name);
+            if (icons[i].name==frutto.name){
+                icons[i].GetComponent<Image>().color=Color.white;
+                fruttipresi[i]=true;
+            }
+        }
+
+        return frutti==ottenuti;
+    }
+
+}
