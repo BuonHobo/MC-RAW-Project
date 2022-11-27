d6fea6e58eb0ee9d716fa423f9cb47bdd8814432
Rainer Cabral Ilao <ilaorainer@gmail.com>
2022-11-27 21:24:13 +0100

    Added Restart Function

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -6,21 +6,30 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject pauseButton;
     public void Pause()
     {
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        pauseButton.SetActive(false);
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        pauseButton.SetActive(true);
     }
 
     public void Home(int sceneID)
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID,LoadSceneMode.Single);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 }
