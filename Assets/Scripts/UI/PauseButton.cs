a92edf87eb2f46499124927f458dacd01992dd8b
Rainer Cabral Ilao <ilaorainer@gmail.com>
2022-11-27 17:03:50 +0100

    Added Pause Menu

--- Assets/Scripts/UI/PauseButton.cs
+++ Assets/Scripts/UI/PauseButton.cs
@@ -3,20 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
+public class Startup : MonoBehaviour {
+ 
+    public static GameObject PauseMenuPanel;
+ 
+    void Start ()
+    {
+        PauseMenuPanel = GameObject.Find("PauseMenu");
+        PauseMenuPanel.SetActive(false);
+    }
+ 
+ }
+
 public class PauseButton : MonoBehaviour
 {
-    private bool paused = false;
-    // Start is called before the first frame update
+    //private static GameObject PauseMenuPanel;
+    //private bool paused = false;
+    // Start is called before the first frame 
+    /*
     void Start()
     {
-
+        //PauseMenuPanel = GameObject.Find("PauseMenu");
+        //PauseMenuPanel.SetActive(false);
     }
+    */
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Cancel") || CrossPlatformInputManager.GetButtonDown("Cancel"))
         {
+            //Time.timeScale = 0;
+            Debug.Log(Startup.PauseMenuPanel);
+
+            /*
             if (!paused)
             {
                 Time.timeScale = 0;
@@ -27,6 +47,7 @@ public class PauseButton : MonoBehaviour
                 Time.timeScale = 1;
                 paused = false;
             }
+            */
         }
     }
 }
