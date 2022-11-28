cb00d28ec9c490c4f9dec49eb739fba278c7f495
buonhobo <naturytropik@gmail.com>
2022-11-28 17:48:08 +0100

    Fixed issue with fruit not being collected

---
+++ Assets/Scripts/Collectibles/CollectibleFruit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleFruit : MonoBehaviour
+{
+    [HideInInspector]
+    public bool wasCollected {get; set;}=false;
+    
+}
--- Assets/Scripts/Collectibles/ItemCollector.cs
+++ Assets/Scripts/Collectibles/ItemCollector.cs
@@ -10,26 +10,24 @@ public class ItemCollector : MonoBehaviour
     //[SerializeField] private AudioSource collectionSoundEffect;
 
     public bool collected { get; private set; } = false;
-    private bool canCollect=true;
 
     [SerializeField] private FruitIndicator indicator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Collectible") && !collected && canCollect)
+        var collectible= collision.gameObject.GetComponent<CollectibleFruit>();
+        
+
+        if (collision.gameObject.CompareTag("Collectible") && !collected && !collectible.wasCollected)
         {
             //collectionSoundEffect.Play();
             var anim = collision.gameObject.GetComponent<Animator>();
             anim.SetBool("Animate", false);
             anim.SetTrigger("Collect");
+
+            collectible.wasCollected=true;
             Destroy(collision.gameObject, 0.5f);
             collected=indicator.addFrutto(collision.gameObject);
-
-            canCollect=false;
-            Invoke("restoreCollect",0.1f);
         }
-    }
-    private void restoreCollect(){
-        canCollect=true;
     }
 }
